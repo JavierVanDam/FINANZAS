57da4c06175673bc8a551f11ac3fe550d722c031
JAVIER VAN DAM <javiervandam@gmail.com>
2022-03-04 11:24:46 -0300

    feature: permisos requeridos en CBVs y FBVs

--- cuentas/views.py
+++ cuentas/views.py
@@ -1,15 +1,20 @@
 from django.views.generic import CreateView, ListView, DetailView
 from django.shortcuts import redirect
-
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from .models import Cuenta
 from django.urls import reverse_lazy
 from .forms import FormularioNuevaCuenta
 from django.shortcuts import render
 #ALTERNATIVA 1. CLASS BASED VIEW CREATE CUENTA
-class CreaCuenta(CreateView):
+
+
+class CreaCuenta(PermissionRequiredMixin, CreateView):
+    permission_required = 'cuentas.add_cuenta'
     model = Cuenta
     fields = '__all__'
     success_url = reverse_lazy('lista_cuentas') #'http://127.0.0.1:8000/base/'
+
 
 class ListaCuentas(ListView):
     model = Cuenta
@@ -20,14 +25,14 @@ class DetalleCuenta(DetailView):
 
 
 #ALTERNATIVA 2. FULL CUSTOMIZADA CREATE CUENTA. CUSTOMIZO LA VIEW Y EL FORM
+@login_required(login_url='login')
+@permission_required('cuentas.add_cuenta', raise_exception=True)
 def nuevaCuentaForm(request):
     form = FormularioNuevaCuenta(request.POST or None)
     if form.is_valid():
         print("FORM VALIDO: ", form.cleaned_data)
         cuentaNueva = Cuenta(**form.cleaned_data)
-
         cuentaNueva.save()
-        # return render(request, template_name='cuentas/cuenta_list.html', context={'form':form})
         return redirect('lista_cuentas')
     else:
         #CUANDO HAY ERRORES EN EL FORM, DJANGO SE ENCARGA AUTOMATICAMENTE DE PONERLO EN EL FORM
