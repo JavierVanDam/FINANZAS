5de4e55388f64578ca8613d186a1b04092b1d57a
JAVIER VAN DAM <javiervandam@gmail.com>
2022-03-10 14:52:20 -0300

    cambios basicones

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,6 @@
+import imp
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -115,3 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+
+
+LOGIN_REDIRECT_URL = 'usuarios:login'
+LOGIN_URL='usuarios:login'
+
--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,10 @@
+import imp
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='base.html')),
     path('admin/', admin.site.urls),
     path('usuario/', include('usuarios.urls')),
     path('cuentas/', include('cuentas.urls')),
